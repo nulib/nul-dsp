b920a3fc6f2647842a82d9601c9e8ad2a6a61b5a
Adam Joseph Arling <aarling@gmail.com>
2024-02-16 15:43:34 -0600

    Wire up with NUL data

--- src/Plotly.jsx
+++ src/Plotly.jsx
@@ -2,32 +2,13 @@ import * as d3 from "d3";
 
 import Plot from "react-plotly.js";
 import React from "react";
-import textAndVectorData from "./data/vectors_tsne_2d_reduced.json";
-
-function unpack(rows, key) {
-  return rows.map(function (row) {
-    return row[key];
-  });
-}
+import textAndVectorData from "./data/vectors_tsne_3d_300_count.json";
 
 const PlotlyTest = () => {
-  const [rows, setRows] = React.useState([]);
-
-  React.useEffect(() => {
-    async function getData() {
-      const response = await d3.csv(
-        "https://raw.githubusercontent.com/plotly/datasets/master/3d-scatter.csv"
-      );
-      console.log("response", response);
-      setRows(response);
-    }
-    getData();
-  }, []);
-
   const trace1 = {
-    x: unpack(rows, "x1"),
-    y: unpack(rows, "y1"),
-    z: unpack(rows, "z1"),
+    x: textAndVectorData.map((item) => item.x),
+    y: textAndVectorData.map((item) => item.y),
+    z: textAndVectorData.map((item) => item.z),
     mode: "markers",
     marker: {
       size: 12,
@@ -38,27 +19,11 @@ const PlotlyTest = () => {
       opacity: 0.8,
     },
     type: "scatter3d",
+    text: textAndVectorData.map((item) => item.title),
+    hoverinfo: "text",
   };
 
-  const trace2 = {
-    x: unpack(rows, "x2"),
-    y: unpack(rows, "y2"),
-    z: unpack(rows, "z2"),
-    mode: "markers",
-    marker: {
-      color: "rgb(127, 127, 127)",
-      size: 12,
-      symbol: "circle",
-      line: {
-        color: "rgb(204, 204, 204)",
-        width: 1,
-      },
-      opacity: 0.8,
-    },
-    type: "scatter3d",
-  };
-
-  const data = [trace1, trace2];
+  const data = [trace1];
   const layout = {
     title: "3D Scatter Plot",
     autosize: true,
@@ -72,14 +37,13 @@ const PlotlyTest = () => {
 
   return (
     <div className="space-y-8 h-[60vh]">
-      {rows?.length > 0 && (
-        <Plot
-          data={data}
-          layout={layout}
-          useResizeHandler
-          style={{ width: "100%", height: "100%" }}
-        />
-      )}
+      <Plot
+        data={data}
+        layout={layout}
+        useResizeHandler
+        style={{ width: "100%", height: "100%" }}
+        onHover={(e) => console.log("hover", e)}
+      />
     </div>
   );
 };
