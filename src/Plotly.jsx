c21bfe31a86450acc69c82b38139220d39aff260
Adam Joseph Arling <aarling@gmail.com>
2024-02-16 16:25:18 -0600

    Hook into event handling to target data point and return NUL Work id

--- src/Plotly.jsx
+++ src/Plotly.jsx
@@ -1,11 +1,31 @@
 import * as d3 from "d3";
 
+import { useEffect, useState } from "react";
+
 import Plot from "react-plotly.js";
-import React from "react";
 import textAndVectorData from "./data/vectors_tsne_3d_300_count.json";
 
 const PlotlyTest = () => {
+  const [plotInstance, setPlotInstance] = useState(null);
+
+  useEffect(() => {
+    if (plotInstance) {
+      // TODO: Stop this from firing multiple times
+      plotInstance.on("plotly_hover", function (data) {
+        const id = data.points[0].id;
+        console.log("id", id);
+      });
+    }
+
+    return () => {
+      if (plotInstance) {
+        plotInstance.removeAllListeners("plotly_hover");
+      }
+    };
+  }, [plotInstance]);
+
   const trace1 = {
+    ids: textAndVectorData.map((item) => item.id),
     x: textAndVectorData.map((item) => item.x),
     y: textAndVectorData.map((item) => item.y),
     z: textAndVectorData.map((item) => item.z),
@@ -42,7 +62,8 @@ const PlotlyTest = () => {
         layout={layout}
         useResizeHandler
         style={{ width: "100%", height: "100%" }}
-        onHover={(e) => console.log("hover", e)}
+        onInitialized={(figure, graphDiv) => setPlotInstance(graphDiv)}
+        onUpdate={(figure, graphDiv) => setPlotInstance(graphDiv)}
       />
     </div>
   );
